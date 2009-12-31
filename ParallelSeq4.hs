a727ff819ad09eee3f60afcc166d9bb2794ee949
Atsuro Hoshino <atsuro@salt.(none)>
2009-12-31 13:47:19 -0800

    Trying to control ugen with n_map.
    Though its not working yet.

--- ParallelSeq4.hs
+++ ParallelSeq4.hs
@@ -3,7 +3,10 @@
 --
 -- This time using @n_map@ to control triggers and parameters of ugen
 -- which making the sound.
---
+-- 
+-- TODO: 
+--  * Write DSL for mapping nodes ... its tedious thing to do.
+--  * Find efficient way for book keeping of nodeID, busID, buffeerID.
 
 module ParallelSeq4 where
 
@@ -19,23 +22,115 @@ para4UGen = out 0 (pan2 osc pos 1) where
     osc = sinOsc ar freq 0 * amp
     pos = control kr "pan" 0.0
     freq = control kr "freq" 440
-    amp = control kr "amp" (dbAmp 80) * env
+    amp = control kr "amp" (dbAmp (-20)) * env
     env = envGen kr envTrig 1 0 1 DoNothing envShape
     envTrig = control kr "trig" 0
     envShape = envPerc 0.01 0.8
 
+-- | Make this controllable!
+bpm :: Num a => a
+bpm = 130
+
 -- | UGen to send trigger.
-trigUGen :: UGen
-trigUGen = undefined
+trigUGen :: IO UGen
+trigUGen = do 
+    durs <- dbufrd (control kr "bufnum" 0) 0 Loop
+    let bus = control kr "bus" 100
+        trigger = tDuty kr (60*durs/bpm) 0 RemoveSynth 1 0
+    return $ out bus trigger 
 
 -- | UGen to send parameters.
-paramUGen :: UGen
-paramUGen = undefined
+paramUGen :: IO UGen
+paramUGen = do 
+    params <- dbufrd (control kr "bufnum" 0) 0 Loop
+    let trigger = control kr "trig" 0 
+        param = demand trigger 0 params
+        bus = control kr "bus" 100
+    return $ out bus param 
 
--- | Setup mappings and sound making ugens.
+ampBus1,freqBus1,trigBus1 :: Num a => a
+ampBus1 = 100
+freqBus1 = 101
+trigBus1 = 102
+
+ampBus2,freqBus2,trigBus2 :: Num a => a
+ampBus2 = 200
+freqBus2 = 201
+trigBus2 = 202
+
+ampBuf1,durBuf1,freqBuf1 :: Num a => a
+ampBuf1 = 10
+durBuf1 = 11
+freqBuf1 = 12
+
+ampBuf2, durBuf2,freqBuf2 :: Num a => a
+ampBuf2 = 20
+durBuf2 = 21
+freqBuf2 = 22
+
+-- | Setup mappings, buffers, and sound making ugens.
 setup :: IO ()
-setup = undefined
+setup = do
+  -- write synthdef files
+  writeSynthdef "para4" para4UGen
+  writeSynthdef "trig" =<< trigUGen
+  writeSynthdef "param" =<< paramUGen
+
+  -- allocate buffers and fill with parameters
+  withSC3 (\fd -> do 
+             reloadSynthdef fd
+             setupBuffers fd
+             soundUGenMappings fd)
+
+-- | Allocates buffers and fills with parameters.
+-- 
+-- XXX: Use bundle instead of calling async and send repeatedly.
+-- 
+setupBuffers :: Transport t => t -> IO ()
+setupBuffers fd = do
+  async fd (b_alloc ampBuf1 (length notes1) 1) 
+  async fd (b_alloc freqBuf1 (length notes1) 1)
+  async fd (b_alloc ampBuf2 (length notes2) 1) 
+  async fd (b_alloc freqBuf2 (length notes2) 1) 
+
+  send fd $ b_setn ampBuf1 [(length notes1, 
+                             map (dbAmp . noteAmp) notes1)]
+  send fd $ b_setn freqBuf1 [(length notes1, 
+                              map (midiCPS . notePitch) notes1)]
+  send fd $ b_setn ampBuf2 [(length notes2, 
+                             map (dbAmp . noteAmp) notes2)]
+  send fd $ b_setn freqBuf2 [(length notes2, 
+                              map (midiCPS . notePitch) notes2)]
+
+-- | Send sound generating ugens and node mapping messages.
+soundUGenMappings :: Transport t => t -> IO ()
+soundUGenMappings fd = do
+  let nId1 = 1000 
+      nId2 = 1001
+      paraAmp1 = 1002
+      paraFreq1 = 1003
+      paraAmp2 = 1004
+      paraFreq2 = 1005
+  send fd $ s_new "para4" nId1 AddToTail 1 [("pan",0.5)]
+  send fd $ s_new "para4" nId2 AddToTail 1 [("pan",-0.5)]
+  send fd $ n_map nId1 
+           [("amp",ampBus1),("freq",freqBus1),("trig",trigBus1)]
+  send fd $ n_map nId2 
+           [("amp",ampBus2),("freq",freqBus2),("trig",trigBus2)]
+
+  send fd $ s_new "param" paraAmp1 AddToHead 1 
+           [("bus",ampBus1),("trig",trigBus1),("bufnum",ampBuf1)]
+  send fd $ s_new "param" paraAmp2 AddToHead 1 
+           [("bus",ampBus2),("trig",trigBus2),("bufnum",ampBuf2)]
 
 -- | Go with player ugen.
 go :: IO ()
-go = undefined
+go = utcr >>= withSC3 . send' . bundle where
+    bundle time = Bundle (UTCr time) 
+     [
+      s_new "trig" 2001 AddToHead 1
+                [("bus",trigBus1),("bufnum",durBuf1)],
+      s_new "trig" 2002 AddToHead 1
+                [("bus",trigBus2),("bufnum",durBuf2)]
+     ]
+                   
