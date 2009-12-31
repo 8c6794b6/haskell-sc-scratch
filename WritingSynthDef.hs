8c8a77931827eee1cd22e69ac87aa4933699228a
Atsuro Hoshino <atsuro@salt.(none)>
2009-12-31 00:45:57 -0800

    Wrote few synthdef examples.

--- WritingSynthDef.hs
+++ WritingSynthDef.hs
@@ -8,6 +8,7 @@ import Sound.SC3
 import Sound.OpenSoundControl
 import System.Environment (getEnvironment)
 import System.FilePath ((</>), (<.>))
+import System.Random
 import qualified Data.ByteString as B
 
 -- | Environmental variable for synthdefs.
@@ -18,8 +19,8 @@ hsc3SynthdefDirEnvVar = "HSC3_SYNTHDEF_DIR"
 -- environmental variable @HSC3_SYNTHDEF_DIR@. Though, to use the
 -- synthdef, one must load the synthdef file explicitly, or execute
 -- @withSC3 reloadSynthdef@ action.
-writeDefFile :: String -> UGen -> IO ()
-writeDefFile name ugen = do
+writeSynthdef :: String -> UGen -> IO ()
+writeSynthdef name ugen = do
   env <- getEnvironment
   let dir = maybe "./" id $ lookup hsc3SynthdefDirEnvVar env
       path = dir </> name <.> "scsyndef"
@@ -30,14 +31,35 @@ writeDefFile name ugen = do
 -- | Reload synthdef directory. If specified, path to the
 -- @HSC3_SYNTHDEF_DIR@ would be used. Otherwise, current directory
 -- would be used.
-reloadSynthDef :: (Transport t) => t -> IO ()
-reloadSynthDef fd = do
+reloadSynthdef :: (Transport t) => t -> IO ()
+reloadSynthdef fd = do
   env <- getEnvironment
   let dir = maybe "./" id $ lookup hsc3SynthdefDirEnvVar env
       path = dir </> ""
   send fd (d_loadDir path)
 
-
 -- | Simple ugen with mono channel sin oscillator.
 simpleUGen :: UGen
 simpleUGen = out 0 (sinOsc AR 440 0 * 0.5)
+
+-- | UGen with its params determined randomly.
+simpleRandomUGen :: IO UGen
+simpleRandomUGen = do
+   freqs <- fmap (take 5) (newStdGen >>= return . randomRs (50,1200))
+   let ugens = out 0 (sinOsc AR (mce freqs) 0 * 0.2)
+   return ugens
+
+-- | UGen with taking parameters.
+simpleParamUGen :: UGen
+simpleParamUGen = out channel oscillator where
+    channel = Control KR "out" 0
+    oscillator = sinOsc AR freq 0 * amp
+    amp = Control KR "amp" 0.3
+    freq = Control KR "freq" 440
+
+-- | Sends new simpleParam UGen with specifying parameters.
+sendSimpleParam :: Int -> Double -> Double -> Double -> IO ()
+sendSimpleParam nodeId channel amp freq = do
+    writeSynthdef "simpleParam" simpleParamUGen
+    withSC3 $ \fd -> send fd (s_new "simpleParam" 
+
