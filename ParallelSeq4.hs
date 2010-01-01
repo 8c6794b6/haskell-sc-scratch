743bdb566b3660deb61db411622a76e321bb50e7
Atsuro Hoshino <atsuro@salt.(none)>
2010-01-01 03:57:47 -0800

    Updating dbufrd parallel synthdef triggering example.
    Un other words, parallel sequencing take 4.1.

--- ParallelSeq4.hs
+++ ParallelSeq4.hs
@@ -37,29 +37,37 @@ bpm = 130
 -- | UGen to send trigger.
 trigUGen :: IO UGen
 trigUGen = do 
-    durs <- dbufrd (control kr "durbuf" 0) 0 NoLoop
-    let bus = control kr "out" 100
-        trigger = tDuty kr (60*durs/bpm) 0 RemoveSynth 1 0
-    return $ out bus trigger 
+  let idx = in' 1 kr (control kr "idx" 0) 
+  durs <- dbufrd (control kr "durbuf" 0) idx NoLoop
+  let bus = control kr "out" 100
+      trigger = tDuty kr (60*durs/bpm) 0 RemoveSynth 1 0
+      idx' = stepper trigger 0 0 
+             (bufFrames kr (control kr "durbuf" 0)) 1 0
+  return $ mce [out bus trigger, out (control kr "idx" 0) idx']
 
 -- | UGen to send parameters.
 paramUGen :: IO UGen
 paramUGen = do 
-    params <- dbufrd (control kr "parambuf" 0) 0 NoLoop
-    let trigger = control kr "trig" 0 
-        param = demand trigger 0 params
-        bus = control kr "out" 100
-    return $ out bus param 
+  let idx = in' 1 kr (control kr "idx" 0)
+  params <- dbufrd (control kr "parambuf" 0) idx NoLoop
+  let trigger = control kr "trig" 0 
+      param = demand trigger 0 params
+      bus = control kr "out" 100
+      idx' = stepper trigger 0 0 
+             (bufFrames kr (control kr "parambuf" 0)) 1 0
+  return $ mce [out bus param, out (control kr "idx" 0) idx']
 
-ampBus1,freqBus1,trigBus1 :: Num a => a
+ampBus1,freqBus1,trigBus1,idxBus1 :: Num a => a
 ampBus1 = 100
 freqBus1 = 101
 trigBus1 = 102
+idxBus1 = 103
 
-ampBus2,freqBus2,trigBus2 :: Num a => a
+ampBus2,freqBus2,trigBus2,idxBus2 :: Num a => a
 ampBus2 = 200
 freqBus2 = 201
 trigBus2 = 202
+idxBus2 = 203
 
 ampBuf1,durBuf1,freqBuf1 :: Num a => a
 ampBuf1 = 10
@@ -91,30 +99,31 @@ setup = do
 -- 
 setupBuffers :: Transport t => t -> IO ()
 setupBuffers fd = do
-
+  now <- utcr
   async fd (b_free ampBuf1)
-  async fd (b_free freqBuf1) 
-  async fd (b_free durBuf1)
-  async fd (b_free ampBuf2)
-  async fd (b_free freqBuf2) 
-  async fd (b_free durBuf2)
-
   async fd (b_alloc ampBuf1 (length notes1) 1) 
+  async fd (b_free freqBuf1) 
   async fd (b_alloc freqBuf1 (length notes1) 1)
+  async fd (b_free durBuf1)
   async fd (b_alloc durBuf1 (length notes1) 1)
+
+  async fd (b_free ampBuf2)
   async fd (b_alloc ampBuf2 (length notes2) 1) 
+  async fd (b_free freqBuf2) 
   async fd (b_alloc freqBuf2 (length notes2) 1) 
+  async fd (b_free durBuf2)
   async fd (b_alloc durBuf2 (length notes2) 1)
 
-  send fd $ b_setn ampBuf1 [
-                   (0, map (dbAmp . (flip (-) 100) . noteAmp) notes1)]
-  send fd $ b_setn freqBuf1 [(0, map (midiCPS . notePitch) notes1)]
-  send fd $ b_setn durBuf1 [(0, map noteDur notes1)]
-
-  send fd $ b_setn ampBuf2 [
-                   (0, map (dbAmp . (flip (-) 100) . noteAmp) notes2)]
-  send fd $ b_setn freqBuf2 [(0, map (midiCPS . notePitch) notes2)]
-  send fd $ b_setn durBuf2 [(0, map noteDur notes2)]
+  send fd $ Bundle (UTCr now) 
+           [
+            b_setn ampBuf1 
+                       [(0, map (dbAmp . (flip (-) 100) . noteAmp) notes1)],
+            b_setn freqBuf1 [(0, map (midiCPS . notePitch) notes1)],
+            b_setn durBuf1 [(0, map noteDur notes1)],
+            b_setn ampBuf2 [(0, map (dbAmp . (flip (-) 100) . noteAmp) notes2)],
+            b_setn freqBuf2 [(0, map (midiCPS . notePitch) notes2)],
+            b_setn durBuf2 [(0, map noteDur notes2)]
+           ]
 
 -- | Send sound generating ugens and node mapping messages.
 soundUGenMappings :: Transport t => t -> IO ()
@@ -125,28 +134,27 @@ soundUGenMappings fd = do
       paraFreq1 = 1003
       paraAmp2 = 1004
       paraFreq2 = 1005
-  send fd $ s_new "para4" nId1 AddToTail 1 [("pan",0.5)]
-  send fd $ s_new "para4" nId2 AddToTail 1 [("pan",-0.5)]
-  send fd $ n_map nId1 
-           [("amp",ampBus1),("freq",freqBus1),("trig",trigBus1)]
-  send fd $ n_map nId2 
-           [("amp",ampBus2),("freq",freqBus2),("trig",trigBus2)]
-
-  send fd $ s_new "param" paraAmp1 AddToHead 1 
-           [("out",ampBus1),("parambuf",ampBuf1)]
-  send fd $ n_map paraAmp1 [("trig",trigBus1)]
-
-  send fd $ s_new "param" paraFreq1 AddToHead 1
-           [("out",freqBus1),("parambuf",freqBuf1)]
-  send fd $ n_map paraAmp1 [("trig",trigBus1)]
-
-  send fd $ s_new "param" paraAmp2 AddToHead 1 
-           [("out",ampBus2),("parambuf",ampBuf2)]
-  send fd $ n_map paraAmp1 [("trig",trigBus2)]
-
-  send fd $ s_new "param" paraFreq2 AddToHead 1
-           [("out",freqBus2),("parambuf",freqBuf2)]
-  send fd $ n_map paraAmp1 [("trig",trigBus2)]
+  now <- utcr
+  send fd $ Bundle (UTCr now) 
+       [
+        s_new "para4" nId1 AddToTail 1 [("pan",0.5)],
+        s_new "para4" nId2 AddToTail 1 [("pan",-0.5)],
+        n_map nId1 [("amp",ampBus1),("freq",freqBus1),("trig",trigBus1)],
+        n_map nId2 [("amp",ampBus2),("freq",freqBus2),("trig",trigBus2)],
+        s_new "param" paraAmp1 AddToHead 1 
+                  [("out",ampBus1),("parambuf",ampBuf1)],
+        n_map paraAmp1 [("trig",trigBus1)],
+        s_new "param" paraFreq1 AddToHead 1
+                  [("out",freqBus1),("parambuf",freqBuf1)],
+        n_map paraFreq1 [("trig",trigBus1)],
+        s_new "param" paraAmp2 AddToHead 1 
+                  [("out",ampBus2),("parambuf",ampBuf2)],
+        n_map paraAmp2 [("trig",trigBus2)],
+        s_new "param" paraFreq2 AddToHead 1
+           [("out",freqBus2),("parambuf",freqBuf2)],
+        n_map paraFreq2 [("trig",trigBus2)],
+        c_set [(idxBus1,0),(idxBus2,0)]
+       ]
 
 -- | Go with player ugen.
 go :: IO ()
@@ -154,18 +162,7 @@ go = utcr >>= withSC3 . send' . bundle where
     bundle time = Bundle (UTCr time) 
      [
       s_new "trig" 2001 AddToHead 1
-                [("out",trigBus1),("durbuf",durBuf1)],
+                [("out",trigBus1),("durbuf",durBuf1),("idx",idxBus1)],
       s_new "trig" 2002 AddToHead 1
-                [("out",trigBus2),("durbuf",durBuf2)]
+                [("out",trigBus2),("durbuf",durBuf2),("idx",idxBus2)]
      ]
-                   
-test1 = 
-    let n = randomRs (200.0, 500.0) (mkStdGen 0)
-    in do { withSC3 (\fd -> do { async fd (b_alloc 10 24 1)
-                           ; send fd (b_setn 10 [(0, take 24 n)]) })
-      ; s <- dseq 3 (mce [0, 3, 5, 0, 3, 7, 0, 5, 9])
-      ; b <- dbrown 5 0 23 1
-      ; p <- dseq dinf (mce [s, b])
-      ; t <- dust KR 10
-      ; r <- dbufrd 10 p Loop
-      ; audition (out 0 (sinOsc AR (demand t 0 r) 0 * 0.1)) }
