efebc0b09f235977159c518861c8d9676dbce0d9
Atsuro Hoshino <atsuro@salt.(none)>
2010-01-01 12:06:02 -0800

    Used notes defined in SimpleNotes.

--- ParallelSeq3.hs
+++ ParallelSeq3.hs
@@ -14,6 +14,7 @@ module ParralelSeq3 where
 
 import Sound.SC3
 import Sound.OpenSoundControl
+import SimpleNotes
 import Reusable
 
 -- | Set groups of scsynth.
@@ -31,26 +32,26 @@ ampBus2 = 200
 freqBus2 = 201
 trigBus2 = 202
 
-data Note = Note { notePitch :: Double,
-                   noteAmp :: Double,
-                   noteDur :: Double }
-          deriving (Eq, Show)
+-- data Note = Note { notePitch :: Double,
+--                    noteAmp :: Double,
+--                    noteDur :: Double }
+--           deriving (Eq, Show)
 
-notes1 :: [Note]
-notes1 = concat $ replicate 2
-    [
-     Note 60 80 0.5, Note 62 78 0.5, Note 64 78 1.0, Note 60 81 1.5,
-     Note 62 76 0.5, Note 64 81 2.0, Note 60 80 1.5, Note 62 81 0.5
-    ]
+-- notes1 :: [Note]
+-- notes1 = concat $ replicate 2
+--     [
+--      Note 60 80 0.5, Note 62 78 0.5, Note 64 78 1.0, Note 60 81 1.5,
+--      Note 62 76 0.5, Note 64 81 2.0, Note 60 80 1.5, Note 62 81 0.5
+--     ]
 
-notes2 :: [Note]
-notes2 =
-    [
-     Note 48 82 1.5, Note 43 75 0.5, Note 48 80 1.5, Note 43 75 0.5,
-     Note 53 81 1.5, Note 48 75 0.5, Note 53 80 1.5, Note 48 75 0.5,
-     Note 48 82 1.5, Note 43 75 0.5, Note 48 80 1.5, Note 43 75 0.5,
-     Note 55 81 1.5, Note 50 75 0.5, Note 55 80 1.5, Note 50 75 0.5
-    ]
+-- notes2 :: [Note]
+-- notes2 =
+--     [
+--      Note 48 82 1.5, Note 43 75 0.5, Note 48 80 1.5, Note 43 75 0.5,
+--      Note 53 81 1.5, Note 48 75 0.5, Note 53 80 1.5, Note 48 75 0.5,
+--      Note 48 82 1.5, Note 43 75 0.5, Note 48 80 1.5, Note 43 75 0.5,
+--      Note 55 81 1.5, Note 50 75 0.5, Note 55 80 1.5, Note 50 75 0.5
+--     ]
 
 -- | UGen making sound.
 paraUGen :: UGen
