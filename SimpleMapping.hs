fe8f344b9404301f3c59daf403035f72c6c02c8e
Atsuro Hoshino <atsuro@salt.(none)>
2010-01-02 13:04:37 -0800

    Tidied up a bit.

--- SimpleMapping.hs
+++ SimpleMapping.hs
@@ -18,7 +18,7 @@ import Data.Generics
 import Data.Data
 import Data.Typeable
 
-import Text.ParserCombinators.Parsec
+-- import Text.ParserCombinators.Parsec
 
 data SCTree = Group NodeId [SCTree]
             | Synth NodeId SynthName [SynthParam]
@@ -40,36 +40,29 @@ data ParamValue = CValue Double
 defaultGroup :: SCTree
 defaultGroup = Group 0 [Group 1 []]
 
+-- | Only working with osc message including synth control parameters.
 parseSCTree :: OSC -> SCTree
-parseSCTree (Message s ds) 
-    | s == "/g_queryTree.reply" = fst (parseSCTree' ds)
+parseSCTree (Message s ds)
+    | s == "/g_queryTree.reply" = fst (parseSCTree' (tail ds))
     | otherwise = error "not a /g_queryTree.reply message"
 parseSCTree _ = error "OSC mssage is not a 'Message', might be 'Bundle'"
 
 parseSCTree' :: [Datum] -> (SCTree,[Datum])
-parseSCTree' [] = error "empty list"
-parseSCTree' (d:ds) = parseGroup ds
-
-parseGroup :: [Datum] -> (SCTree,[Datum])
-parseGroup (Int x:Int y:ds) 
+parseSCTree' [] = error "empty list passed to \"parseSCTree'\""
+parseSCTree' (Int x:Int y:ds)
     | y < 0 = parseSynth x ds
     | otherwise = (Group x tree,ds')
-    where
-      (tree,ds') = parseSCTreeFor y ds []
+    where (tree,ds') = parseSCTreeFor y ds []
 
 parseSCTreeFor :: Int -> [Datum] -> [SCTree] -> ([SCTree],[Datum])
 parseSCTreeFor left ds ts
     | left == 0 = (ts,ds)
-    | otherwise = parseSCTreeFor (left-1) ds' (ts++[t]) 
-    where
-      (t,ds') = parseGroup ds
-
-getNextNode = undefined
+    | otherwise = parseSCTreeFor (left-1) ds' (ts++[t])
+    where (t,ds') = parseSCTree' ds
 
 parseSynth :: NodeId -> [Datum] -> (SCTree,[Datum])
 parseSynth nid (String name:Int num:ds) = (Synth nid name params,ds')
-    where
-      (params,ds') = parseParamsFor num [] ds
+    where (params,ds') = parseParamsFor num [] ds
 
 parseParamsFor :: Int -> [SynthParam] -> [Datum] -> ([SynthParam],[Datum])
 parseParamsFor 0 ps ds = (ps,ds)
@@ -79,14 +72,14 @@ parseParamsFor n ps ds = parseParamsFor (n-1) (ps++[ps']) ds'
 parseParam :: [Datum] -> (SynthParam,[Datum])
 parseParam (String n:d:ds) = (SynthParam n d',ds)
     where
-      d' = case d of 
+      d' = case d of
              Float x -> CValue x
              String s -> BusId (read (tail s))
 
 -- | Sample message returned from scsynth server, without group other
--- than default. 
+-- than default.
 oscList1 :: OSC
-oscList1 = Message "/g_queryTree.reply" 
+oscList1 = Message "/g_queryTree.reply"
                 [Int 1, -- containing control parameters
                  Int 1,Int 2, -- default group, with 2 child elements
                  Int 1000,Int (-1),
@@ -105,14 +98,14 @@ oscList1 = Message "/g_queryTree.reply"
                  String "out",Float 0.0]
 
 scTree1 :: SCTree
-scTree1 = Group 1 
-               [Synth 1000 "simplePercSine" 
+scTree1 = Group 1
+               [Synth 1000 "simplePercSine"
                 [SynthParam "sustain" (CValue 0.8),
                  SynthParam "trig" (CValue 0),
                  SynthParam "amp" (CValue 0.1),
                  SynthParam "freq" (CValue 440),
                  SynthParam "out" (CValue 0)],
-                Synth 1000 "simplePercSine" 
+                Synth 1000 "simplePercSine"
                 [SynthParam "sustain" (CValue 0.8),
                  SynthParam "trig" (CValue 0),
                  SynthParam "amp" (CValue 0.1),
@@ -120,14 +113,14 @@ scTree1 = Group 1
                  SynthParam "out" (CValue 0)]]
 
 oscList2 :: OSC
-oscList2 = 
-    Message "/g_queryTree.reply" 
+oscList2 =
+    Message "/g_queryTree.reply"
                 [Int 1, -- containing control parameters
                  Int 0,Int 1, -- root node, with 1 child element
                  Int 1,Int 5, -- default group, with 5 child element
                  Int 2,Int 0, -- group 2, no child element
                  Int 3,Int 4, -- group 3, 4 child elements.
-                 Int 1002,Int (-1), -- node 1002, 
+                 Int 1002,Int (-1), -- node 1002,
                  String "param",Int 4, -- name is "param", 4 control params.
                  String "idx",String "c103", -- idx, from control bus 103.
                  String "parambuf",Float 10.0,
@@ -191,10 +184,10 @@ oscList2 =
                  String "gain",Float 1.0,
                  String "bus",Float 204.0]
 
--- | Testing syb. 
+-- | Testing syb.
 getAllNames :: Data a => a -> [SynthName]
-getAllNames = everything (++) ([] `mkQ` f) 
-    where 
+getAllNames = everything (++) ([] `mkQ` f)
+    where
       f (Synth _ n _) = [n]
       f a = []
 
