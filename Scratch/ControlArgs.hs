aa7ed5b130c6d1f7ab14e8d17b1afa4c29c51c8a
Atsuro Hoshino <atsuro@salt.(none)>
2010-01-19 10:24:18 -0800

    Added control args.

--- Scratch/ControlArgs.hs
+++ Scratch/ControlArgs.hs
@@ -6,15 +6,31 @@ module Scratch.ControlArgs where
 
 import Reusable
 
-out = "out" @= 0 
-in' = "in" @= 0
-bus = "bus" @= 0
-lfoBus = "lfobus" @= 0
-freq = "freq" @= 440
-ffreq = "ffreq" @= 440
 add = "add" @= 0
-mul = "mul" @= 0.1
+amp = "amp" @= 0.3
+bufnum = "bufnum" @= 1
+bus = "bus" @= 0
+cut = "cut" @= 4000
+cutoff = "cutoff" @= 10000
+decay = "decay" @= 4
+delay = "delay" @= 0.2
+ffreq = "ffreq" @= 440
+freq = "freq" @= 440
 gain = "gain" @= 1
-preGain = "preGain" @= 1
-postGain = "postGain" @= 1
+gate = "gate" @= 0
+in' = "in" @= 0
+i_bus = "i_bus" @= 10
+i_start = "i_start" @= 1000
+i_end = "i_end" @= 500
+i_time = "i_time" @= 1
+lfoBus = "lfobus" @= 0
+mul = "mul" @= 0.1
+out = "out" @= 0 
 pan = "pan" @= 0
+postGain = "postGain" @= 1
+preGain = "preGain" @= 1
+rate = "rate" @= 1
+rez = "rez" @= 1
+rezz = "rezz" @= 1
+sustain = "sustain" @= 1
+trig = "trig" @= 1
