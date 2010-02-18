6c1ddda9f2c01ca685cb276efdef312dd5e631f7
8c6794b6 <8c6794b6@gmail.com>
2010-02-19 00:36:45 +0200

    Added controller ugen with lfNoise2.

--- Etude/Additive01.hs
+++ Etude/Additive01.hs
@@ -11,24 +11,27 @@ import System.Random
 
 import Sound.OpenSoundControl
 import Sound.SC3
+import qualified Sound.SC3.UGen.Noise.Unsafe as N
 
 import SCQuery
 import SCTree
 import Reusable
 import qualified Scratch.ControlArgs as A
-import Scratch.Pat.P5
+-- import Scratch.Pat.P5
 
 -- | Type synonym for @line@ and @xLine@.
 type LineUGen = Rate -> UGen -> UGen -> UGen -> DoneAction -> UGen
 
 updateSynthdefs :: IO ()
-updateSynthdefs = 
-    mapM_ (uncurry writeSynthdef) synthdefs >> withSC3 reloadSynthdef
+updateSynthdefs = do
+  mapM_ (\(n,u) -> writeSynthdef n =<< u) synthdefs 
+  withSC3 reloadSynthdef
     where 
-      synthdefs = [("anOsc", anOsc),
-                   ("aLine", aLine),
-                   ("aLag", aLag),
-                   ("anXLine", anXLine)]
+      synthdefs = [("anOsc", pure anOsc),
+                   ("aLine", pure aLine),
+                   ("aLag", pure aLag),
+                   ("aNoise", aNoise),
+                   ("anXLine", pure anXLine)]
 
 numOsc :: Int
 numOsc = 128
@@ -89,10 +92,14 @@ tree =
 amps :: [SCTree]
 amps = zipWith4 mkLag
        ampIds ampBusses (repeat 0.02) (repeat 1)
+-- amps = zipWith5 mkNoise
+--        ampIds ampBusses (repeat 0.02) (repeat 0.001) (repeat 0.2)
 
 freqs :: [SCTree]
-freqs = zipWith4 mkLag
-        freqIds freqBusses (repeat 440) (repeat 1)
+freqs = zipWith5 mkNoise
+        freqIds freqBusses (repeat 440) (repeat 20) (repeat 2)
+-- freqs = zipWith4 mkLag
+--         freqIds freqBusses (repeat 440) (repeat 1)
 
 pans :: [SCTree]
 pans = zipWith4 mkLag
@@ -110,6 +117,12 @@ mkLag nId bId val dur =
     Synth nId "aLag"
           ["out":=fromIntegral bId, "val":=val, "dur":=dur]
 
+mkNoise :: NodeId -> BusId -> Double -> Double -> Double -> SCTree
+mkNoise nId bId val range freq = 
+    Synth nId "aNoise"
+          ["out":=fromIntegral bId, 
+           "val":=val, "range":=range, "freq":=freq]
+
 anOsc :: UGen
 anOsc = out 0 $ pan2 (sinOsc ar A.freq 0 * A.amp) A.pan 1
 
@@ -118,6 +131,11 @@ aLine = out A.out $ line kr A.start A.end A.dur DoNothing
 
 aLag :: UGen
 aLag = out A.out $ lag A.val A.dur 
+
+aNoise :: IO UGen
+aNoise = do
+  n <- lfNoise2 kr A.freq 
+  return $ out A.out $ A.val + (n * A.range / 2)
 
 anXLine :: UGen
 anXLine = out A.out $ xLine kr A.start A.end A.dur DoNothing
