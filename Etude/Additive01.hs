e339555893ce54c7e9769f58769a093cf6959688
8c6794b6 <8c6794b6@gmail.com>
2010-02-20 14:12:17 +0200

    Tidied up a bit.
    + Added group IDs
    + Tidied up few functions.

--- Etude/Additive01.hs
+++ Etude/Additive01.hs
@@ -11,13 +11,12 @@ import System.Random
 
 import Sound.OpenSoundControl
 import Sound.SC3
-import qualified Sound.SC3.UGen.Noise.Unsafe as N
 
 import SCQuery
 import SCTree
+import SCSched
 import Reusable
 import qualified Scratch.ControlArgs as A
--- import Scratch.Pat.P5
 
 -- | Type synonym for @line@ and @xLine@.
 type LineUGen = Rate -> UGen -> UGen -> UGen -> DoneAction -> UGen
@@ -27,17 +26,24 @@ updateSynthdefs = do
   mapM_ (\(n,u) -> writeSynthdef n =<< u) synthdefs 
   withSC3 reloadSynthdef
     where 
-      synthdefs = [("anOsc", pure anOsc),
-                   ("aLine", pure aLine),
-                   ("aLag", pure aLag),
-                   ("aNoise", aNoise),
-                   ("anXLine", pure anXLine)]
+      synthdefs = 
+          [("anOsc", pure anOsc),
+           ("aLine", pure aLine),
+           ("aLag", pure aLag),
+           ("aNoise2", aNoise2)]
 
 numOsc :: Int
 numOsc = 128
 
 oscIds :: [NodeId]
 oscIds = mkIds 1001
+
+ampGroup, freqGroup, panGroup, oscGroup, effectGroup :: NodeId
+ampGroup = 10
+freqGroup = 11
+panGroup = 12
+oscGroup = 20
+effectGroup = 30
 
 ampIds :: [NodeId]
 ampIds = mkIds 2001
@@ -48,9 +54,6 @@ freqIds = mkIds 3001
 panIds :: [NodeId]
 panIds = mkIds 4001
 
-mkIds :: Int -> [Int]
-mkIds str = enumFromTo str (str+numOsc-1)
-
 ampBusses :: [Int]
 ampBusses = mkIds 1
 
@@ -59,6 +62,9 @@ freqBusses = mkIds 1001
 
 panBusses :: [Int]
 panBusses = mkIds 2001
+
+mkIds :: Int -> [Int]
+mkIds str = enumFromTo str (str+numOsc-1)
 
 setAmps :: [Double] -> IO ()
 setAmps = setVals ampIds
@@ -70,24 +76,28 @@ setPans :: [Double] -> IO ()
 setPans = setVals panIds
 
 setVals :: [NodeId] -> [Double] -> IO ()
-setVals ns ps = 
-    mapM_ (\(n, p) -> withSC3 $ \t -> send t $ n_set n [("val", p)])
-         (zip ns ps)
+setVals = setParams "val"
 
 setDurs :: [NodeId] -> [Double] -> IO ()
-setDurs ns ps = 
-    mapM_ (\(n,p) -> withSC3 $ \t -> send t $ n_set n [("dur",p)])
+setDurs = setParams "dur"
+
+setParams :: String -> [NodeId] -> [Double] -> IO ()
+setParams name ns ps = 
+    mapM_ (\(n,p) -> withSC3 $ \t -> send t $ n_set n [(name,p)])
           (zip ns ps)
+
+setParams' :: String -> [NodeId] -> [Double] -> [OSC]
+setParams' name ns ps = zipWith (\n p ->  n_set n [(name,p)]) ns ps
 
 tree :: SCTree
 tree = 
   Group 0 
    [Group 1 
-    [Group 10 amps,
-     Group 11 freqs,
-     Group 12 pans,
-     Group 20 oscs,
-     Group 30 []]]
+    [Group ampGroup amps,
+     Group freqGroup freqs,
+     Group panGroup pans,
+     Group oscGroup oscs,
+     Group effectGroup []]]
 
 amps :: [SCTree]
 amps = zipWith4 mkLag
@@ -96,10 +106,10 @@ amps = zipWith4 mkLag
 --        ampIds ampBusses (repeat 0.02) (repeat 0.001) (repeat 0.2)
 
 freqs :: [SCTree]
-freqs = zipWith5 mkNoise
-        freqIds freqBusses (repeat 440) (repeat 20) (repeat 2)
--- freqs = zipWith4 mkLag
---         freqIds freqBusses (repeat 440) (repeat 1)
+-- freqs = zipWith5 mkNoise2
+ --         freqIds freqBusses (repeat 440) (repeat 20) (repeat 2)
+freqs = zipWith4 mkLag
+        freqIds freqBusses (repeat 440) (repeat 1)
 
 pans :: [SCTree]
 pans = zipWith4 mkLag
@@ -117,9 +127,9 @@ mkLag nId bId val dur =
     Synth nId "aLag"
           ["out":=fromIntegral bId, "val":=val, "dur":=dur]
 
-mkNoise :: NodeId -> BusId -> Double -> Double -> Double -> SCTree
-mkNoise nId bId val range freq = 
-    Synth nId "aNoise"
+mkNoise2 :: NodeId -> BusId -> Double -> Double -> Double -> SCTree
+mkNoise2 nId bId val range freq = 
+    Synth nId "aNoise2"
           ["out":=fromIntegral bId, 
            "val":=val, "range":=range, "freq":=freq]
 
@@ -132,10 +142,19 @@ aLine = out A.out $ line kr A.start A.end A.dur DoNothing
 aLag :: UGen
 aLag = out A.out $ lag A.val A.dur 
 
-aNoise :: IO UGen
-aNoise = do
+aNoise2 :: IO UGen
+aNoise2 = do
   n <- lfNoise2 kr A.freq 
   return $ out A.out $ A.val + (n * A.range / 2)
 
-anXLine :: UGen
-anXLine = out A.out $ xLine kr A.start A.end A.dur DoNothing
+partialPitches :: [Double] -> [Double]
+partialPitches ps = zipWith (*) muls ps'
+    where muls = concatMap (replicate (length ps)) [1..]
+          ps' = cycle (map midiCPS ps)
+
+-- Note of pitches.
+-- [35,47,52,56,59,64] 
+-- [36,48,52,55,60,64]
+
+-- Rhythm: 7/8
+-- x-x-xxx- x-xxx- 
