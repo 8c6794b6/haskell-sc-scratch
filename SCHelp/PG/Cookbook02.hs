933fdcca71b3d8f085f83ad52037475be65e1e66
8c6794b6 <8c6794b6@gmail.com>
2010-02-24 22:48:27 +0200

    Added stub for cookbook02.

---
+++ SCHelp/PG/Cookbook02.hs
@@ -0,0 +1,18 @@
+------------------------------------------------------------------------------
+-- | 
+-- Module      : SCHelp.PG.Cookbook02
+-- Copyright   : 8c6794b6 <8c6794b6@gmail.com>
+-- Licens      : BSD3
+-- Maintainer  : 8c6794b6
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Exercise for implementing pattern sequences shown in 
+-- /PG_Cookbook02_Manipulating_Patterns/
+-- 
+
+module SCHelp.PG.Cookbook02 (
+  ) where
+
+import Sound.OpenSoundControl
+import Sound.SC3
