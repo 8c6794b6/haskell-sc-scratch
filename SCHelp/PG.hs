1887fe65ca47c4a13b14d0c2b9ea12d3073f07cc
8c6794b6 <8c6794b6@gmail.com>
2010-02-25 00:18:25 +0200

    Added stub modul.

---
+++ SCHelp/PG.hs
@@ -0,0 +1,20 @@
+------------------------------------------------------------------------------
+-- |
+-- Module      : SCHelp.PG
+-- Copyright   : 8c6794b6 <8c6794b6@gmail.com>
+-- License     : BSD3
+-- Maintainer  : 8c6794b6
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Exercise for implementing pattern sequences shown in
+-- Pattern Cookbook of supercollider help file.
+--
+
+module SCHelp.PG 
+    (module SCHelp.PG.Cookbook01,
+     module SCHelp.PG.Cookbook02
+    ) where
+
+import SCHelp.PG.Cookbook01
+import SCHelp.PG.Cookbook02
