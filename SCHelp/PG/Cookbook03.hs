c2f03f97c748bcec6926846a3d5052f6165411d1
8c6794b6 <8c6794b6@gmail.com>
2010-03-05 20:59:08 +0900

    Wrote simple key press control example.

--- SCHelp/PG/Cookbook03.hs
+++ SCHelp/PG/Cookbook03.hs
@@ -10,12 +10,29 @@
 -- Exercise for implementing pattern sequences shown in
 -- /PG_Cookbook03_External_Control/.
 --
---
 
-module SCHelp.PG.Cookbook03 where
+module SCHelp.PG.Cookbook03 (
+    main,
+
+    -- * Control of parameters by MIDI or HID
+    -- $controlByHID
+    runByHID,
+    playByKey,
+    playNote,
+
+    -- * Triggering a pattern by a GUI
+    -- $triggeringByGUI
+    runByGUI,
+
+    -- * Triggering a pattern by signal amplitude
+    -- $triggeringBySignalAmplitude
+    runBySignalAmplitude
+
+    ) where
 
 import Control.Applicative
 import Control.Monad
+import Data.Char
 
 import Sound.OpenSoundControl
 import Sound.SC3
@@ -28,18 +45,47 @@ import SCSched
 main :: IO ()
 main = undefined
 
--- $controlByMIDIOrHID
--- 
--- Translation of @Phidkey@ and @PhidSlot@ pattern in sclang.
--- 
-
--- $triggeringByGUI
--- 
--- Using GTK.
+-- $controlByHID
 --
+-- Translation of @Phidkey@ and @PhidSlot@ pattern in sclang.
+-- Triggering sending of message with key press. 'a' is midiNote 60,
+-- and 'w' is 61, as so on until 'k', 72.
+--
+runByHID :: IO ()
+runByHID = getChar >>= playByKey >> runByHID
+
+playByKey :: Char -> IO ()
+playByKey 'a' = playNote 60
+playByKey 'w' = playNote 61
+playByKey 's' = playNote 62
+playByKey 'e' = playNote 63
+playByKey 'd' = playNote 64
+playByKey 'f' = playNote 65
+playByKey 't' = playNote 66
+playByKey 'g' = playNote 67
+playByKey 'y' = playNote 68
+playByKey 'h' = playNote 69
+playByKey 'u' = playNote 70
+playByKey 'j' = playNote 71
+playByKey 'k' = playNote 72
+playByKey 'q' = return ()
+playByKey _ = return ()
+
+playNote :: Double -> IO ()
+playNote note = withSC3 $ \fd -> do
+   send fd (s_new "simpleSynth" (-1) AddToTail 1
+            [("dur",1),("freq",midiCPS note)])
+  
+-- $triggeringByGUI
+--
+-- Using GTK. Triggering sending of
+--
+runByGUI :: IO ()
+runByGUI = undefined
 
 -- $triggeringBySignalAmplitude
--- 
+--
 -- A bit trickier, in sclang.
--- 
-
+--
+runBySignalAmplitude :: IO ()
+runBySignalAmplitude = undefined
