6f67e45c290790380a094a1fb79e6a3b109958d9
8c6794b6 <8c6794b6@gmail.com>
2010-03-11 18:20:43 +0900

    Added stub file.

---
+++ SCHelp/PG/Cookbook06.hs
@@ -0,0 +1,32 @@
+------------------------------------------------------------------------------
+-- |
+-- Module      : SCHelp.PG.Cookbook06
+-- Copyright   : 8c6794b6 <8c6794b6@gmail.com>
+-- License     : BSD3
+-- Maintainer  : 8c6794b6
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Exercise for implementing pattern sequences shown in
+-- /PG_Cookbook06_Phrase_Network/.
+--
+
+module SCHelp.PG.Cookbook06 where
+
+import Sound.OpenSoundControl
+import Sound.SC3
+
+import Reusable
+import SCSched
+import SCTree
+import SCQuery
+
+sawpulse :: UGen
+sawpulse = undefined
+
+phraseTempo :: Num a => a
+phraseTempo = 128
+
+phrase01 = undefined
+phrase02 = undefined
+phrase03 = undefined
