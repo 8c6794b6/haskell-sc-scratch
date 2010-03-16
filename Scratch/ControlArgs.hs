54cad2fed160e23c1e5cccc734222c595883e34b
8c6794b6 <8c6794b6@gmail.com>
2010-03-16 17:20:50 +0900

    Added more arguments.

--- Scratch/ControlArgs.hs
+++ Scratch/ControlArgs.hs
@@ -10,6 +10,8 @@ accent = "accent" @= 0
 add = "add" @= 0
 amp = "amp" @= 0.3
 attack = "attack" @= 0.1
+baseFreqBuf = "baseFreqBuf" @= 0
+bufBase = "bufBase" @= 0
 bufnum = "bufnum" @= 1
 bus = "bus" @= 0
 cut = "cut" @= 4000
@@ -20,6 +22,7 @@ delay = "delay" @= 0.2
 dur = "dur" @= 1
 ffreq = "ffreq" @= 440
 freq = "freq" @= 440
+freqlag = "freqlag" @= 1
 end = "end" @= 0
 gain = "gain" @= 1
 gate = "gate" @= 0
@@ -30,14 +33,18 @@ i_end = "i_end" @= 500
 i_time = "i_time" @= 1
 lfoBus = "lfobus" @= 0
 mul = "mul" @= 0.1
+mw = "mw" @= 0
 out = "out" @= 0 
 pan = "pan" @= 0
+plfofreq = "plfofreq" @= 1
 postGain = "postGain" @= 1
+preamp = "preamp" @= 1
 preGain = "preGain" @= 1
 range = "range" @= 1
 rate = "rate" @= 1
 rez = "rez" @= 1
 rezz = "rezz" @= 1
+rq = "rq" @= 0
 start = "start" @= 0
 stretch = "stretch" @= 1
 sustain = "sustain" @= 1
