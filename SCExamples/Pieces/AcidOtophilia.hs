39db141042b4ec5250a0e1ba7ce61eae7aa7084e
8c6794b6 <8c6794b6@gmail.com>
2010-03-16 16:16:20 +0900

    Added stub module for exercise.
    Trying to implement acid_otophilia example in haskell.

---
+++ SCExamples/Pieces/AcidOtophilia.hs
@@ -0,0 +1,19 @@
+------------------------------------------------------------------------------
+-- | 
+-- Module      : SCExamples.Pieces.AcidOtopholia
+-- Copyright   : 8c6794b6 <8c6794b6@gmail.com>
+-- License     : BSD3
+-- Maintainer  : 8c6794b6
+-- Stability   : experimental
+-- Portability : portable
+--
+-- Exercise for implementing sc3 example from
+-- /acid_otophilia.scd/.
+--
+-- 
+
+module SCExamples.Pieces.AcidOtophilia where
+
+import Sound.OpenSoundControl
+import Sound.SC3
+
