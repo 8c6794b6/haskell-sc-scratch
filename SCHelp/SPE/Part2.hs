48519059bcdc381fa1b7f6fb10369612a5a0bf79
8c6794b6 <8c6794b6@gmail.com>
2010-03-20 21:07:34 +0900

    Added stub for SPE example part2.

---
+++ SCHelp/SPE/Part2.hs
@@ -0,0 +1,23 @@
+------------------------------------------------------------------------------
+-- |
+-- 
+-- Exercise from /Stream-Patterns-Events1/.
+--
+-- 
+
+module SCHelp.SPE.Part2 where
+
+import Sound.OpenSoundControl
+import Sound.SC3
+import Sound.SC3.Wing
+import qualified Sound.SC3.Wing.ControlArg as A
+
+runPart2 :: IO ()
+runPart2 = undefined
+
+setPart2 :: IO ()
+setPart2 = undefined
+
+help_SPE2 :: IO UGen
+help_SPE2 = undefined
+
