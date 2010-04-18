d0d3852b1a76685bb05dd52254c954ca321fd67e
8c6794b6 <8c6794b6@gmail.com>
2010-04-19 00:42:36 +0200

    Start wriring hsc3-wing-midi module.

---
+++ hsc3-wing-midi/Sound/SC3/Wing/MIDI.hs
@@ -0,0 +1,47 @@
+------------------------------------------------------------------------------
+-- |
+-- Module      : Sound.SC3.Wing.MIDI
+-- Copyright   : 8c6794b6 <8c6794b6@gmail.com>
+-- License     : BSD3
+-- Maintainer  : 8c6794b6
+-- Stability   : experimental
+-- Portability : non-portable
+--
+-- MIDI Extension to hsc3.
+--
+
+module Sound.SC3.Wing.MIDI 
+    ( withMIDI
+    ) where
+
+
+import Control.Applicative ((<$>), (<*>))
+import Control.Exception (bracket)
+import Control.Monad (forever)
+import Sound.Alsa.Sequencer
+
+-- | Like @withEvents@ found in Sound.ALSA.Sequencer module, invoke
+-- given action with MIDI input event.
+withMIDI :: String -> String -> (Event -> IO a) -> IO a
+withMIDI clientName portName action = do
+  bracket (getSeq clientName) close worker
+    where
+      worker aseq = do
+        _ <- getWriteablePort aseq portName
+        forever (action =<< event_input aseq)
+
+-- | Get sequencer handler with given client name.
+getSeq :: String -> IO SndSeq
+getSeq cname = do
+  aseq <- open default_seq_name open_input Block
+  set_client_name aseq cname
+  return aseq
+
+-- | Get input port for given sequencer client.
+getWriteablePort :: SndSeq -> String -> IO Port
+getWriteablePort s name =
+    create_simple_port s name cap type_midi_generic
+        where cap = caps [cap_write, cap_subs_write, cap_sync_write]
+
+
+
