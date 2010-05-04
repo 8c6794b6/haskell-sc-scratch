e5cb3c75cae37a65d74afc8f5f971ac0caf77cd9
8c6794b6 <8c6794b6@gmail.com>
2010-05-04 15:14:43 -0400

    Modified to book keeping nodeId for each track.

--- Scratch/Midi/PlayWithLily.hs
+++ Scratch/Midi/PlayWithLily.hs
@@ -18,37 +18,47 @@ import Scratch.Midi.FromLily
 import Scratch.Scheduling1
 import Scratch.UGen
 
-data NodeState = NodeState {offsetId :: Int, nodeMap :: IntMap Int}
-                 deriving (Eq, Show)
+data NodeState = NodeState {
+      nodeSynth :: String, 
+      currentId :: Int, 
+      nodeMap :: IntMap Int
+    } deriving (Eq, Show)
 
 initNodeState :: NodeState
-initNodeState = NodeState 8000 IntMap.empty
+initNodeState = NodeState  "simpleGated" 8000 IntMap.empty
 
-seqTupToOSC :: (Num a) => [(d, [(Int, a)])] -> [(d, [OSC])]
-seqTupToOSC ts = evalState (mapM newOrFreeList ts) initNodeState
+-- seqTupToOSC :: (Num a) => [(d, [(Int, a)])] -> [(d, [OSC])]
+-- seqTupToOSC ts = evalState (mapM newOrFreeList ts) initNodeState
 
 newOrFree :: (Num a) => (Int, a) -> State NodeState OSC
 newOrFree (p, 0) = do
-  NodeState cur m <- get
+  NodeState _ cur m <- get
   let nid = maybe (-1) id $ IntMap.lookup p m
   return $ n_set nid [("gate", 0)]
 newOrFree (p, _) = do
-  NodeState cur m <- get
+  NodeState name cur m <- get
   let m' = IntMap.insert p cur m
-  put $ NodeState (cur+1) m'
-  return $ s_new "simpleGated" cur AddToTail 1
+  put $ NodeState name (cur+1) m'
+  return $ s_new name cur AddToTail 1
              [("freq", midiCPS $ fromIntegral p)]
 
 newOrFreeList :: (Num a) => (d, [(Int, a)]) -> State NodeState (d, [OSC])
 newOrFreeList (t, ps) = do
-  st@(NodeState cur m) <- get
+  st@(NodeState name cur m) <- get
   let (osc, st') = runState (mapM newOrFree ps) st
   put st'
   return (t, osc)
 
 oscTracks :: IO (SeqEvent OSC)
-oscTracks = seqTupToOSC . foldr merge [] . extractTracks <$> theTracks
+oscTracks = seqTupToOSC initNodeState . foldr merge [] . extractTracks <$> theTracks
+
+seqTupToOSC :: (Num a) => NodeState -> [(d, [(Int, a)])] -> [(d, [OSC])]
+seqTupToOSC st ts = evalState (mapM newOrFreeList ts) st
 
 midiToGated :: FilePath -> IO (SeqEvent OSC)
-midiToGated file =
-  seqTupToOSC . foldr merge [] . extractTracks <$> (MIDIFileLoad.fromFile file)
+midiToGated file = foldr merge [] . 
+                   zipWith seqTupToOSC sts . 
+                   extractTracks <$> 
+                   MIDIFileLoad.fromFile file
+    where
+      sts = map (\n -> NodeState "simpleGated" (n*1000) IntMap.empty) [8..]
