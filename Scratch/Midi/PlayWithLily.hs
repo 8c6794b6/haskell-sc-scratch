79610a6049d3d0ec51db0f4b3eeb23c4009e7c8a
8c6794b6 <8c6794b6@gmail.com>
2010-05-04 13:27:03 -0400

    Tidied up a bit.

--- Scratch/Midi/PlayWithLily.hs
+++ Scratch/Midi/PlayWithLily.hs
@@ -6,13 +6,12 @@ import Control.Monad.State
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IntMap
 
-
 import Sound.SC3
 import Sound.OpenSoundControl
 import qualified Sound.MIDI.File as MIDIFile
 import qualified Sound.MIDI.File.Load as MIDIFileLoad
 
-import Scratch.Midi.FromLily 
+import Scratch.Midi.FromLily
 import Scratch.Scheduling1
 import Scratch.UGen
 
@@ -27,26 +26,26 @@ seqTupToOSC ts = evalState (mapM newOrFreeList ts) initNodeState
 
 newOrFree :: (Num a) => (Int, a) -> State NodeState OSC
 newOrFree (p, 0) = do
-  NodeState cur m <- get 
+  NodeState cur m <- get
   let nid = maybe (-1) id $ IntMap.lookup p m
   return $ n_set nid [("gate", 0)]
 newOrFree (p, _) = do
   NodeState os m <- get
   let m' = IntMap.insert p os m
   put $ NodeState (os+1) m'
-  return $ s_new "simpleGated" os AddToTail 1 
+  return $ s_new "simpleGated" os AddToTail 1
              [("freq", midiCPS $ fromIntegral p)]
 
 newOrFreeList :: (Num a) => (d, [(Int, a)]) -> State NodeState (d, [OSC])
-newOrFreeList (t, ps) = do 
-  st@(NodeState cur m) <- get 
+newOrFreeList (t, ps) = do
+  st@(NodeState cur m) <- get
   let (osc, st') = runState (mapM newOrFree ps) st
-  put st' 
+  put st'
   return (t, osc)
 
 oscTracks :: IO (SeqEvent OSC)
 oscTracks = seqTupToOSC . foldr merge [] . extractTracks <$> theTracks
 
 midiToGated :: FilePath -> IO (SeqEvent OSC)
-midiToGated file = 
+midiToGated file =
   seqTupToOSC . foldr merge [] . extractTracks <$> (MIDIFileLoad.fromFile file)
