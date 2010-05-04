fd955795f66f1f769a6807f4260fdbfda2164476
8c6794b6 <8c6794b6@gmail.com>
2010-05-04 04:36:17 -0400

    Added adhoc function to play multi track midi file.

--- Scratch/Midi/FromLily.hs
+++ Scratch/Midi/FromLily.hs
@@ -2,21 +2,21 @@
 ------------------------------------------------------------------------------
 -- | Trying to read midi file created by lilypond and convert it to
 -- data which is sequentially playable by haskell.
--- 
+--
 -- TODO:
--- 
--- * Play each individual track with chosing name of synthdef. 
--- 
+--
+-- * Play each individual track with chosing name of synthdef.
+--
 -- * Modify to work with gated synthdef, with converting 0 velocity
 --   message.
--- 
+--
 
 module Scratch.Midi.FromLily where
 
 import Control.Applicative
 import Control.Arrow
 import Control.Monad (join)
-import Data.Maybe 
+import Data.Maybe
 
 import qualified Data.EventList.Relative.TimeBody as TimeBody
 import qualified Numeric.NonNegative.Class as NonNegativeClass
@@ -61,29 +61,29 @@ isMIDIEvent _ = False
 midiToOSC :: MIDIEvent.T -> Maybe OSC
 midiToOSC (MIDIEvent.MIDIEvent _) = undefined
 
-collectMIDIEvent :: TimeBody.T MIDIEvent.ElapsedTime MIDIEvent.T 
+collectMIDIEvent :: TimeBody.T MIDIEvent.ElapsedTime MIDIEvent.T
                  -> [(MIDIEvent.ElapsedTime, [MIDIEvent.T])]
 collectMIDIEvent = TimeBody.toPairList . TimeBody.collectCoincident
                    . TimeBody.filter isMIDIEvent
 
 -- | Extract pitch and velocity from midi event.
-midiEvent2Tuple :: (Num a, Num b) => MIDIEvent.T -> Maybe (a, b)
-midiEvent2Tuple (MIDIEvent.MIDIEvent (Channel.Cons _ body)) = 
+midiEventToTuple :: (Num a, Num b) => MIDIEvent.T -> Maybe (a, b)
+midiEventToTuple (MIDIEvent.MIDIEvent (Channel.Cons _ body)) =
     case body of
-      Channel.Voice (Voice.NoteOn p v) -> 
-          Just (fromIntegral . Voice.fromPitch $ p, 
+      Channel.Voice (Voice.NoteOn p v) ->
+          Just (fromIntegral . Voice.fromPitch $ p,
                 fromIntegral . Voice.fromVelocity $ v)
-      Channel.Voice (Voice.NoteOff p v) -> 
-          Just (fromIntegral . Voice.fromPitch $ p, 
+      Channel.Voice (Voice.NoteOff p v) ->
+          Just (fromIntegral . Voice.fromPitch $ p,
                 fromIntegral . Voice.fromVelocity $ v)
       _ -> Nothing
 
-midiEventsToTuple :: (Num a, Num b, Num c) => 
-                     TimeBody.T MIDIEvent.ElapsedTime MIDIEvent.T -> 
+timeBodyToTuple :: (Num a, Num b, Num c) =>
+                     TimeBody.T MIDIEvent.ElapsedTime MIDIEvent.T ->
                     [(a, [(b, c)])]
-midiEventsToTuple = map f . collectMIDIEvent 
-    where f (a, b)= (fromInteger . MIDIEvent.fromElapsedTime $ a, 
-                     catMaybes $ map midiEvent2Tuple b)
+timeBodyToTuple = map f . collectMIDIEvent
+    where f (a, b)= (fromInteger . MIDIEvent.fromElapsedTime $ a,
+                     catMaybes $ map midiEventToTuple b)
 
 shiftTime :: (Num t) => [(t, a)] -> [(t, a)]
 shiftTime [] = []
@@ -92,23 +92,30 @@ shiftTime ((t1,v1):(t2,v2):es) = (t2,v1): shiftTime ((t2,v2):es)
 
 midiToPV :: (Fractional a, Num b, Num c) => MIDIFile.T -> [(a, [(b, c)])]
 midiToPV = map (first (/ 384)) . shiftTime . concat .
-           map midiEventsToTuple . tail . getTracks
-
-extractTrack :: (Fractional a, Num b, Num c) => 
-                MIDIFile.Track -> 
-               [(a, [(b, c)])]
-extractTrack = undefined
+           map timeBodyToTuple . tail . getTracks
 
 percSynthNew :: String -> Int -> (Double, Double) -> Maybe OSC
 percSynthNew _ _ (p,0) = Nothing
 percSynthNew name grp (p,v) = Just osc
-    where osc = s_new name (-1) AddToTail grp [("freq", midiCPS p)] 
-
+    where osc = s_new name (-1) AddToTail grp [("freq", midiCPS p)]
 
 playPercSynth :: FilePath -> BPM -> String -> IO ()
-playPercSynth file bpm name = 
-    runSeqOSC bpm =<< 
+playPercSynth file bpm name =
+    runSeqOSC bpm =<<
     map (second (catMaybes . map (percSynthNew name 1))) .
     midiToPV <$> MIDIFileLoad.fromFile file
 
-                    
+et :: (Fractional t) =>
+      String ->
+      TimeBody.T MIDIEvent.ElapsedTime MIDIEvent.T ->
+      [(t, [OSC])]
+et name = map (second (catMaybes . map (percSynthNew name 1)) .
+               first (/ 384)) . shiftTime . timeBodyToTuple
+
+-- | Plays multi track midi file with single percussive synthdef.
+playTracks :: FilePath -> BPM -> String -> IO ()
+playTracks file bpm name =
+    runSeqOSC bpm =<<
+    foldr merge [] .
+    map (et name) . tail . getTracks <$>
+    MIDIFileLoad.fromFile file
