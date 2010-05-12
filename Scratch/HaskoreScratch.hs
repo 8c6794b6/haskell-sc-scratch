fe1660d16dc699aa5e35038d983028f230438bbe
8c6794b6 <8c6794b6@gmail.com>
2010-05-12 10:52:07 -0400

    Tidied up a bit.
    Noticed that forgot to implement for rests.

--- Scratch/HaskoreScratch.hs
+++ Scratch/HaskoreScratch.hs
@@ -16,11 +16,37 @@ import Sound.SC3.Wing.ScheduleSimple
 type MelodyT a = Haskore.Melody.T a
 type ListT = MCL.T
 
-main :: IO ()
-main = undefined
+-- | Type synonym for function to convert a Note to list of OSC. 
+type NoteToOSCs a = Double -- ^ Duration
+                  -> Int -- ^ Pitch, in MIDI
+                  -> Maybe a -- ^ Note attribute
+                  -> [OSC] 
 
+noteToSeqEv :: NoteToOSCs a -> MelodyT a -> SeqEvent OSC
+noteToSeqEv f n@(Primitive x) = 
+     [(noteToBeat n, f (noteToBeat n) (toInt $ noteToPitch n) (noteToAttr n))]
+noteToSeqEv f n@(Serial xs) = concatMap (noteToSeqEv f) xs
+noteToSeqEv f n@(Parallel xs) = foldr (//) [] (map (noteToSeqEv f) xs)
+noteToSeqEv f n@(Control x y) = []
+
+seri :: [MCL.T control a] -> MCL.T control a
 seri = serial 
+
+para :: [MCL.T control a] -> MCL.T control a
 para = parallel
+
+noteToAttr :: MelodyT a -> Maybe a
+noteToAttr (Primitive x) = fmap (getVal noteAttrs) x' where Atom _ x' = x
+noteToAttr _ = Nothing
+
+noteToBeat :: Fractional a => MelodyT b -> a
+noteToBeat (Primitive x) = 4 * toNumber d where Atom d _ = x
+
+
+f1 :: NoteToOSCs a
+f1 _ pch _ = [s_new "simplePitched" (-1) AddToTail 1 
+              [("freq", midiCPS $ fromIntegral pch)]]
+
 
 aaa :: MelodyT [(String,Double)]
 aaa = seri 
@@ -42,27 +68,7 @@ line1 = para [l1, l2, l3]
 line2 :: MelodyT ()
 line2 = para [l1, l2, l3]
     where
-      l1 = seri [e 7 sn (), e 7 sn (), e 7 sn (), e 7 sn ()]
-      l2 = seri [g 6 hn (), g 6 hn ()]
-      l3 = seri [c 5 qn ()]
-
-noteToAttr :: MelodyT a -> Maybe a
-noteToAttr (Primitive x) = fmap (getVal noteAttrs) x' where Atom _ x' = x
-noteToAttr _ = Nothing
-
-noteToBeat :: Fractional a => MelodyT b -> a
-noteToBeat (Primitive x) = 4 * toNumber d where Atom d _ = x
-
-type NoteToOSCs a = Double -> Int -> Maybe a -> [OSC]
-
-f1 :: NoteToOSCs a
-f1 _ pch _ = [s_new "simplePitched" (-1) AddToTail 1 
-              [("freq", midiCPS $ fromIntegral pch)]]
-
-noteToSeqEv :: NoteToOSCs a -> MelodyT a -> SeqEvent OSC
-noteToSeqEv f n@(Primitive x) = 
-     [(noteToBeat n, f (noteToBeat n) (toInt $ noteToPitch n) (noteToAttr n))]
-noteToSeqEv f n@(Serial xs) = concatMap (noteToSeqEv f) xs
-noteToSeqEv f n@(Parallel xs) = foldr merge [] (map (noteToSeqEv f) xs)
-noteToSeqEv f n@(Control x y) = []
+      l1 = seri [e 6 sn (), d 6 sn (), c 6 sn (), d 6 sn ()]
+      l2 = seri [g 5 en (), g 5 en ()]
+      l3 = seri [c 4 qn ()]
 
