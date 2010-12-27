6250c7b208def94a860b50886329c6d686ff7d56
8c6794b6 <8c6794b6@gmail.com>
2010-12-28 02:32:06 +1300

    Add help messages for cli

--- fts/src/Main.hs
+++ fts/src/Main.hs
@@ -5,32 +5,44 @@ module Main where
 
 import Data.Data (Data, Typeable)
 
-import System.Console.CmdArgs (cmdArgs, modes)
-
+import System.Console.CmdArgs
+  ( (&=)
+  , cmdArgs
+  , help
+  , modes
+  , summary
+  , typ
+  , typDir )
 import qualified Fts.Command.Index as I
 import qualified Fts.Command.Serve as S
 
 -- | Data type for parsing command line args.
 data Fts
   = Serve { port :: Int
-          , dbPath :: FilePath
-          , templatePath :: FilePath }
-  | Index { dbPath :: FilePath
-          , targetPath :: FilePath }
+          , db :: FilePath
+          , template :: FilePath }
+  | Index { db :: FilePath
+          , target :: FilePath }
   deriving (Eq, Show, Data, Typeable)
 
 -- | Parse args, run command.
 main :: IO ()
 main = do
     arg <- cmdArgs $ modes [server, indexer]
+           &= summary "fts: simple full text search with tokyo dystopia"
     case arg of
       Index d t   -> I.run d t
       Serve p d t -> S.run p d t
 
 -- | Default setting for @fts serve@.
 server :: Fts
-server = Serve 8000 "db" "templates"
+server = Serve
+  { port = 8000 &= help "port number to serve" &= typ "NUM"
+  , db = "db" &= help "path to indexed database" &= typDir
+  , template = "templates" &= help "path to template directory" &= typDir }
 
 -- | Default setting for @fts index@.
 indexer :: Fts
-indexer = Index "db" "target"
+indexer = Index
+  { db = "db" &= help "output path for indexed data" &= typDir
+  , target = "target" &= help "path for target html files" &= typDir }
