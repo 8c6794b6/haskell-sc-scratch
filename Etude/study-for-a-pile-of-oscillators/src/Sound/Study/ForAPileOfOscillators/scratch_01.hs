67c43bf1e9b34ec93a913ff995285434dd4d1c5b
8c6794b6 <8c6794b6@gmail.com>
2011-07-20 15:35:22 +0900

    Minor tidy up
    Signed-off-by: 8c6794b6 <8c6794b6@gmail.com>

--- Etude/study-for-a-pile-of-oscillators/src/Sound/Study/ForAPileOfOscillators/scratch_01.hs
+++ Etude/study-for-a-pile-of-oscillators/src/Sound/Study/ForAPileOfOscillators/scratch_01.hs
@@ -1,13 +1,13 @@
 ------------------------------------------------------------------------------
 --
--- This code won't compile, it's use is to select each block of code and 
+-- This code won't compile, it's use is to select each block of code and
 -- send to ghci interactively.
--- 
+--
 -- Select block of code (let or do), and hit C-M-x from emacs.
 -- Or, each line could be sent to ghci with C-c C-C.
 --
 -- Requires inf-haskell-send-region and inf-haskell-send-line.
--- 
+--
 ------------------------------------------------------------------------------
 
 -- Import modules
@@ -16,23 +16,23 @@
 :set -XNoMonomorphismRestriction
 
 -- sdef :: String -> UGen -> IO ()
-let sdef name ug = withSC3 $ \fd ->
-  async fd (d_recv $ synthdef name ug)
+let sdef name ug = withSC3 $ \fd -> do
+  { async fd (d_recv $ synthdef name ug) }
 
 -- hatch :: String -> [(String, Double)] -> IO ()
-let hatch name ps = withSC3 $ \fd ->
-  send fd (s_new name (-1) AddToTail 1 ps)
+let hatch name ps = withSC3 $ \fd -> do
+  { send fd (s_new name (-1) AddToTail 1 ps) }
 
 -- snew :: String -> AddAction -> Int -> [(String, Double)] -> IO ()
 let snew name aaction target ps = withSC3 $ \fd ->
   send fd (s_new name (-1) aaction target ps)
 
-let st = withSC3 $ \fd -> do 
+let st = withSC3 $ \fd -> do
       { send fd status
       ; wait fd "/status.reply" }
-                          
+
 -- Where can be used inside let, with ";" at the end of each clause.
-    
+
 let y1 = pan2 sig ("pan"=:0) 1 where
       sig = decay2 tr 1e-9 380e-3 * pulse ar freq pw * 0.3 * release;
       freq = ("freq"=:880);
@@ -40,48 +40,48 @@ let y1 = pan2 sig ("pan"=:0) 1 where
            dust 'd' kr (lfdNoise3 'n' kr 0.5 * 0.5 + 0.5);
       pw = lfdNoise3 'd' kr 2.137 * 0.5 + 0.5;
       release = xLine kr 1 1e-3 ("rel"=:20) RemoveSynth;
-      
+
 audition $ out 0 y1
-      
-let z1 = pan2 sig ("pan"=:0) 1 where      
-      sig = xLine kr 1 1e-4 120e-3 RemoveSynth * 
+
+let z1 = pan2 sig ("pan"=:0) 1 where
+      sig = xLine kr 1 1e-4 120e-3 RemoveSynth *
             (pulse ar ("freq"=:79) 0.2 +
              whiteNoise 'z' ar) * 0.3
-            
+
 audition $ out 0 z1
 
 let z2 = pan2 sig ("pan"=:0) 1 where
       sig = envGen kr 1 1 0 1 RemoveSynth shape *
-            sinOsc ar (xLine kr 80 20 100e-3 DoNothing) 
+            sinOsc ar (xLine kr 80 20 100e-3 DoNothing)
             (line kr 0 pi 180e-3 DoNothing) * 0.8;
       shape = env [0,0,1,1,0] [0,5e-3,20e-3,280e-3] [EnvNum (-10)] (-1) 0
-      
-audition $ out 0 z2 
 
-let z3 = pan2 sig ("pan"=:0) 1 where      
+audition $ out 0 z2
+
+let z3 = pan2 sig ("pan"=:0) 1 where
       sig = envGen kr 1 1 0 1 RemoveSynth shape *
-            sinOsc ar (("freq"=:7809.8)*fenv) 0 * 
+            sinOsc ar (("freq"=:7809.8)*fenv) 0 *
             decay2 (dust 'd' kr dfreq) 1e-3 20e-3;
       shape = env [0,0,1,0] [0,5e-3,800e-3] [EnvNum (-10)] (-1) 0;
       dfreq = line kr 62 0 1 DoNothing;
       fenv = xLine kr 1 0.25 1 DoNothing
-      
+
 audition $ out 0 z3
 
 let m1 = mrg [replaceOut 0 (in' 1 ar 0*a),replaceOut 1 (in' 1 ar 1*a)] where
       a = ("amp"=:1) `lag` ("lag"=:2)
-      
+
 -- Send synthdefs
 mapM (uncurry sdef)
   [("y1",(out 0 y1)),("m1",m1)
   ,("z1",(out 0 z1)),("z2",(out 0 z2)),("z3",(out 0 z3))]
-  
+
 -- Group mapping
 let g = Group 1
         [Group 11 [Synth (-1) "m1" ["amp":=0.8]]
         ,Group 10 []]
-in  withSC3 $ addNode 0 g        
-        
+in  withSC3 $ addNode 0 g
+
 -- Now I understand why rd was wrting like this.
 let a1 pch = do
       { ds <- runPIO $ pforever $ pchoose 1 $ take 16
@@ -91,7 +91,7 @@ let a1 pch = do
       ; rs <- runPIO $ pforever (pchoose 1 [8,10..25])
       ; let f d p r = snew "foo" AddToHead 11
                    [("freq", midiCPS (d+pch)),("pan", p),("rel",r)]
-      ; in  sequence_ $ zipWith3 f (take 16 ds) ps rs }
+      ; sequence_ $ zipWith3 f (take 16 ds) ps rs }
 
 let ampa1 val = withSC3 $ \fd -> send fd $ n_set 11 [("amp",val)]
 ampa1 0.2
@@ -104,29 +104,28 @@ mapM_ a1 [36,34,32]
 mapM_ a1 [38,36,34]
 mapM_ a1 [34,32,30]
 
--- Sequence a1s. 
+-- Sequence a1s.
 let b1 fac = sequence_ $
     zipWith (\ps t -> mapM_ a1 ps >> threadDelay (floor $ fac * t * 1e6))
     [[46,41,36], [41,36,31], [36,31,26], [36,34,32]]
     [3, 5, 4, 4]
 
 -- Fork the sequence, hold ThreadId so that it could be killed later.
-    
+
 e <- initEnv 1
 
 t1 <- forkIO $ forever (b1 1.6)
 killThread t1
 
-let b2 = do 
+let b2 = do
       { hs <- runPIO $ pforever $ pcycle [0,0,1,0]
       ; bs <- runPIO $ pforever $ pseq 1 [1,0,0,pchoose 1 [0,1]]
       ; let f h b | b == 1 = [s_new "z2" (-1) AddToHead 10 [("pan",0.2)]]
                   | h == 1 = [s_new "z1" (-1) AddToHead 10 [("pan",-0.1)]]
                   | otherwise = [s_new "z3" (-1) AddToHead 10 [("pan",-0.2)]]
-      ;     g m = withSC3 (\fd -> send fd (Bundle immediately m) >> 
+      ;     g m = withSC3 (\fd -> send fd (Bundle immediately m) >>
                                   threadDelay (floor $ 4 * 1e5))
       ; in  forM_ (zipWith f hs bs) g }
-      
+
 t2 <- forkIO b2
 killThread t2
-
--- Scratch/Arrows/A1.hs
+++ Scratch/Arrows/A1.hs
@@ -14,16 +14,40 @@ module A1 where
 
 import Control.Arrow
 import Sound.SC3
+import Sound.SC3.ID
+import Sound.SC3.Lepton
 
 addA f g = proc x -> do
   y <- f -< x
   z <- g -< x
   returnA -< y + z
 
--- foo = do 
+ug1 = proc f -> do
+  f' <- flip (sinOsc kr) 0 -< f
+  freq <- (+800) <<< (*800) -< f'
+  out 0 <<< (*0.3) <<< flip (sinOsc ar) 0 -< freq
+
+ug2 :: UGen -> UGen
+ug2 f = out 0 (pan2 (limiter flt 1 0.25) pos 1) where
+  flt  = ringz sig cf q
+  -- flt  = rlpf sig 2200 q
+  cf   = lfdNoise3 'g' kr 8e-1 * 1 + 880
+  q    = lfdNoise3 'q' kr 3e-2 * 0.025 + 0.0250201
+  sig  = mix $ sinOsc ar 0 (mce [frq, frq*1.5, frq*2]) * 0.3 * amp
+  frq  = ffnd + fidx
+  ffnd = lfdNoise3 'f' kr f * 3200 + 3200
+  fidx = sinOsc kr 1423.32 0 * (lfdNoise3 'm' kr 0.125 * 8 + 7.132)
+  amp  = envGen kr hit 1 0 1 DoNothing shp
+  shp  = envPerc atk dcy
+  atk  = lfdNoise3 'a' kr (1/37) * 3e-1 + 3e-1
+  dcy  = lfdNoise3 'l' kr (1/35) * 3e-1 + 3e-1
+  hit  = impulse kr 3 0 + dust 'd' kr 1.85
+  pos  = lfdNoise3 'p' kr 0.251
+
+-- foo = do
 --   let s = sinOsc ar
 --   0 => phase s
 --   lfdNoise3 kr 8 => freq s
 --   return $ out 0 s
-  
--- a1 = \f -> sinOsc ar f 0 >>^ out 0 
+
+-- a1 = \f -> sinOsc ar f 0 >>^ out 0
