d4ff1841f24dd51c64e88bdee37df599a9a2581b
8c6794b6 <8c6794b6@gmail.com>
2011-08-16 22:43:55 +0900

    Add keyboard shortcut for node browser
    Use jquery.hotkeys to map keyboard event to user interface action.
    Show server status at top of right area.
    
    Signed-off-by: 8c6794b6 <8c6794b6@gmail.com>

--- Scratch/Happstack/NodeBrowser/NodeBrowser.hs
+++ Scratch/Happstack/NodeBrowser/NodeBrowser.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-|
 
 Module      : $Header$
@@ -8,7 +9,29 @@ Maintainer  : 8c6794b6@gmail.com
 Stability   : unstable
 Portability : non-portable
 
-Web interface to browse synthdef database.
+Web interface to browse synth nodes in running scsynth server.
+
+/TODO/:
+
+* Add js hot key support for:
+
+    * Moving focus to right area and left area.
+
+    * Showing help of which key does what.
+
+* Add "dump current setting" feature.
+
+* Add feature for adding new nodes.
+
+   * Enable adding group node with specified add action.
+
+   * Enable adding synth node with specified add action.
+
+* Add ui for setting control bus, c_set message.
+
+* Add ui for buffer
+
+    * Use buffer, what kind of operation would be convinient?.
 
 -}
 module NodeBrowser where
@@ -21,66 +44,16 @@ import Data.Monoid (mconcat)
 import Data.Time.LocalTime
 
 import Control.Monad.Trans (liftIO)
+import Data.Generics.Uniplate.Operations
 import Happstack.Server hiding (body)
 import Sound.OpenSoundControl
 import Sound.SC3
 import Sound.SC3.Lepton
 import Text.Blaze.Html5 hiding (base,map,summary)
-import Text.Blaze.Html5.Attributes hiding
-  (dir,id,title,form,style,span,summary)
+import Text.Blaze.Html5.Attributes hiding (dir,id,title,form,style,span,summary)
 
 import qualified Text.Blaze.Html5.Attributes as A
-
-instance ToHtml SCNode where
-  toHtml (Group gid ns) = do
-    div ! 
-      class_ (toValue "group_node") ! 
-      A.id (toValue $ "node_" ++ show gid) $ do
-        div ! class_ (toValue "group_id") $ do
-          span ! class_ (toValue "group_id_num") $ toHtml $ show gid
-          span ! class_ (toValue "group_id_symbol") $ toHtml " group"
-          when (gid /= 0) $ do 
-            span ! class_ (toValue "synth_free") $ do
-              button ! onclick (toValue $ "n_free(" ++ show gid ++ ")") $ 
-                toHtml "free"
-        div ! class_ (toValue "group_children") $ do
-          mapM_ toHtml ns
-  toHtml (Synth nid defn ps) = do
-    div 
-      ! class_ (toValue "synth_node") 
-      ! A.id (toValue $ "node_" ++ show nid) $ do
-      div ! class_ (toValue "synth_id") $ do
-        span ! class_ (toValue "synth_id_num") $ toHtml $ show nid
-        span ! class_ (toValue "synth_id_name") $ do
-          -- XXX: get this link from command line arg.
-          a ! href (toValue $ "http://localhost:8002/synthdef/" ++ defn) $ do
-            toHtml defn
-        span ! class_ (toValue "synth_free") $ do
-          button ! onclick (toValue $ "n_free(" ++ show nid ++ ")") $ 
-            toHtml "free"
-      div ! class_ (toValue "synth_params") $ do
-        mapM_ toHtml ps
-
-instance ToHtml SynthParam where
-  toHtml (n:=v) = do
-    span ! class_ (toValue "param_value") $ do
-      span ! class_ (toValue "param_name") $ toHtml (n ++ ":")
-      span !
-        class_ (toValue "param_value_num") $ do
-          input ! 
-            type_ (toValue "text") !
-            A.name (toValue n) !
-            value (toValue $ show v) !
-            size (toValue "5") !
-            onchange (toValue "n_set(this)")
-  toHtml (n:<-v) = do
-    span ! class_ (toValue "param_control_bus") $ do
-      span ! class_ (toValue "param_name") $ toHtml (n ++ ":")
-      span ! class_ (toValue "param_control_bus_num") $ toHtml ('c':show v)
-  toHtml (n:<=v) = do
-    span ! class_ (toValue "param_audio_bus") $ do
-      span ! class_ (toValue "param_name") $ toHtml (n ++ ":")
-      span ! class_ (toValue "param_audio_bus_num") $ toHtml ('a':show v)
+import qualified Prelude
 
 main :: IO ()
 main = withSC3 $ \fd -> do
@@ -105,45 +78,108 @@ serve :: MVar SCNode -> IO ()
 serve mv = simpleHTTP nullConf $ msum
   [ dir "favicon.ico" $ notFound $ toResponse ()
   , dir "n_set" $ path $ \nid -> nSet mv (read nid)
+  , dir "n_map" $ path $ \nid -> nMap mv (read nid)
+  , dir "n_mapa" $ path $ \nid -> nMapa mv (read nid)
   , dir "n_free" $ path $ \nid -> nFree mv (read nid)
+  , dir "nodeDetail" $ path $ \nid -> nodeDetail mv (read nid)
   , dir "echo" echo
-  , dir "js" $ serveDirectory EnableBrowsing [] "js"    
+  , dir "js" $ serveDirectory EnableBrowsing [] "js"
   , dir "css" $ serveDirectory EnableBrowsing [] "css"
-  , showTree mv  
+  , showTree mv
   ]
 
 showTree :: MVar SCNode -> ServerPartT IO Response
 showTree mv = do
   tree <- liftIO $ readMVar mv
-  ok $ toResponse $ do 
-    preEscapedString "<!doctype html>" 
+  statReply <- liftIO $ withSC3 $ \fd -> do
+    send fd status
+    wait fd "/status.reply"
+  ok $ toResponse $ do
+    preEscapedString "<!doctype html>"
     html $ do
       head $ do
-        title $ toHtml "showTree"
-        meta !
-          httpEquiv (toValue "Content-type") !
-          content (toValue "text/html;charset=UTF-8")
+        title $ "showTree"
+        meta ! httpEquiv "Content-type" ! content "text/html;charset=UTF-8"
         css
         js
       body $ do
-        div ! class_ (toValue "root") $ toHtml tree
+        div ! class_ "wrapper" $ do
+          div ! class_ "root" $ toHtml tree
+          div ! class_ "right" $ rightPart statReply
+        script ! type_ "text/javascript" $ "init()"
 
-nSet :: MVar SCNode -> Int -> ServerPartT IO Response
-nSet mv nid = do
+rightPart :: OSC -> Html
+rightPart statMsg = do
+  div ! class_ "right_wrapper" $ do
+    div ! class_ "right_header" $ showStat statMsg
+    div ! class_ "right_main" $ do
+      div $ helpMessage
+    div ! class_ "right_footer" $ return ()
+
+showStat :: OSC -> Html
+showStat msg = case msg of
+  Message "/status.reply"
+    (_:Int nU:Int nS:Int nG:Int nI:Float cA:Float cP:Double sN:Double sA:_) ->
+      div ! class_ "synth_stat" $ do
+        ul ! class_ "synth_stat_elems" $ do
+          li $ do
+            span ! class_ "synth_stat_name" $ "ugens: "
+            toHtml $ show nU ++ ", "
+            span ! class_ "synth_stat_name" $ " synths: "
+            toHtml $ show nS ++ ", "
+            span ! class_ "synth_stat_name" $ " groups: "
+            toHtml $ show nG ++ ", "
+            span ! class_ "synth_stat_name" $ " instr: "
+            toHtml $ show nI
+          li $ do
+            span ! class_ "synth_stat_name" $ "cpu : "
+            toHtml $ show cA ++ " / " ++ show cP
+          li $ do
+            span ! class_ "synth_stat_name" $ "sample rate : "
+            toHtml $ show sN ++ " / " ++ show sA
+
+withParamValues :: Read t1
+                => (t -> [(String,t1)] -> OSC) -> (String -> String)
+                -> MVar SCNode -> t -> ServerPartT IO Response
+withParamValues f g mv nid = do
   rq <- askRq
   let paramStrings = mkParam $ rqQuery rq
-      paramValues = map (\(a,b) -> (a,read b)) paramStrings
-      msg = n_set nid paramValues
+      paramValues = map (\(a,b) -> (a,read $ g b)) paramStrings
+      msg = f nid paramValues
   liftIO $ withSC3 $ \fd -> do
     send fd msg
+    print msg
     modifyMVar_ mv (const $ getRootNode fd)
   ok $ toResponse $ show msg
+
+nSet :: MVar SCNode -> Int -> ServerPartT IO Response
+nSet = withParamValues n_set id
+
+nMap :: MVar SCNode -> Int -> ServerPartT IO Response
+nMap = withParamValues n_map tail
+
+nMapa :: MVar SCNode -> Int -> ServerPartT IO Response
+nMapa = withParamValues n_mapa tail
 
 echo :: ServerPartT IO Response
 echo = do
   rq <- askRq
   liftIO $ print rq
   ok $ toResponse $ show rq
+
+nFree :: MVar SCNode -> Int -> ServerPartT IO Response
+nFree mv nid = do
+  liftIO $ withSC3 $ \fd -> do
+    send fd $ n_free [nid]
+    modifyMVar_ mv (const $ getRootNode fd)
+  showTree mv
+
+nodeDetail :: MVar SCNode -> Int -> ServerPartT IO Response
+nodeDetail mv nid = do
+  t <- liftIO $ readMVar mv
+  let node = Prelude.head $ universeBi [n|n <- universeBi t, nodeId n == nid]
+  ok $ toResponse $ do
+    div $ synthNodeHtml node True
 
 mkParam :: String -> [(String,String)]
 mkParam qs | null qs = []
@@ -155,25 +191,92 @@ mkParam qs | null qs = []
             y | null zs = (z,[]) | otherwise = (z,tail zs)
             (z,zs) = break (== '=') y'
 
-nFree :: MVar SCNode -> Int -> ServerPartT IO Response
-nFree mv nid = do
-  liftIO $ withSC3 $ \fd -> do
-    send fd $ n_free [nid]
-    modifyMVar_ mv (const $ getRootNode fd)
-  showTree mv
+instance ToHtml SCNode where
+  toHtml (Group gid ns) = do
+    div !
+      class_ "group_node" !
+      A.id (toValue $ "node_" ++ show gid) $ do
+        div ! class_ "group_id" $ do
+          span ! class_ "group_id_num" $ toHtml $ show gid
+          span ! class_ "group_id_symbol" $ " group"
+          when (gid /= 0) $ do
+            span ! class_ "synth_free" $ do
+              button ! onclick (toValue $ "n_free(" ++ show gid ++ ")") $
+                "free"
+        div ! class_ "group_children" $ do
+          mapM_ toHtml ns
+  toHtml n@(Synth nid defn ps) = synthNodeHtml n False
+
+synthNodeHtml :: SCNode -> Bool -> Html
+synthNodeHtml (Group _ _) _ = return ()
+synthNodeHtml (Synth nid defn ps) detailed = do
+  let mainDiv = mkSynthMain nid detailed
+  mainDiv $ do
+    div ! class_ "synth_id" $ do
+      span ! class_ "synth_id_num" $ toHtml $ show nid
+      span ! class_ "synth_id_name" $ do
+        -- XXX: get this link from command line arg.
+        a ! href (toValue $ "http://localhost:8002/synthdef/" ++ defn) $ do
+          toHtml defn
+      span ! class_ ("synth_free") $ do
+        button ! onclick (toValue $ "n_free(" ++ show nid ++ ")") $
+          "free"
+    when detailed $
+      div ! class_ ("synth_params") $ do
+        mapM_ toHtml ps
+
+mkSynthMain :: Int -> Bool -> Html -> Html
+mkSynthMain nid detailed h
+  | detailed =
+    div ! class_ "synth_node_detail" !
+    A.id (toValue $ "node_" ++ show nid) $ h
+  | otherwise =
+    div ! class_ "synth_node" !
+    A.id (toValue $ "node_" ++ show nid ++ "_tree") !
+    onclick (toValue $ "node_detail(" ++ show nid ++ ")") $ h
+
+instance ToHtml SynthParam where
+  toHtml (n:=v)  = mkParamHtml n (show v)
+  toHtml (n:<-v) = mkParamHtml n ('c':show v)
+  toHtml (n:<=v) = mkParamHtml n ('a':show v)
+
+mkParamHtml :: String -> String -> Html
+mkParamHtml n val = do
+  div ! class_ "synth_param" $ do
+    div ! class_ "param_value" $ do
+      div ! class_ "param_name" $ toHtml (n ++ ":")
+      span ! class_ "param_value_num" $ do
+        input !
+          type_ "text" !
+          A.name (toValue n) !
+          value (toValue val) !
+          size "15" !
+          onchange "param_changed(this)"
 
 css :: Html
-css = 
+css =
   link !
-  rel (toValue "stylesheet") ! 
-  type_ (toValue "text/css") ! 
-  href (toValue "css/style.css")
+  rel "stylesheet" !
+  type_ "text/css" !
+  href "css/style.css"
 
 js :: Html
 js = do
   script !
-    type_ (toValue "text/javascript") !
-    src (toValue "js/jquery-1.6.2.min.js") $ return ()
+    type_ "text/javascript" !
+    src "js/jquery-1.6.2.min.js" $ return ()
   script !
-    type_ (toValue "text/javascript") !
-    src (toValue "js/nb.js") $ return ()
+    type_ "text/javascript" !
+    src "js/jquery.hotkeys.js" $ return ()
+  script !
+    type_ "text/javascript" !
+    src "js/nb.js" $ return ()
+
+helpMessage :: Html
+helpMessage = do
+  p "node browser"
+  p "key bindings: "
+  ul $ do
+    li $ "j - move focused node down"
+    li $ "k - move focused node up"
+    li $ "i - focus first input in right area"
--- Scratch/Happstack/NodeBrowser/js/nb.js
+++ Scratch/Happstack/NodeBrowser/js/nb.js
@@ -1,16 +1,18 @@
 function echo(val) {
   $.ajax({
     type: "POST",
-    url:  "/echo?val=" + val
+    url: "/echo?val=" + val
   });
 }
 
 function n_free(nid) {
   $.ajax({
     type: "POST",
-    url:  "/n_free/" + nid
+    url: "/n_free/" + nid,
+    success: function() {
+      $('#node_'+nid).remove();
+    }
   });
-  $('#node_'+nid).remove();
 }
 
 function n_set(me) {
@@ -20,6 +22,83 @@ function n_set(me) {
   var val = $(me)[0].value;
   $.ajax({
     type: "POST",
-    url:  "/n_set/" + nid + "?" + key + "=" + val
+    url: "/n_set/" + nid + "?" + key + "=" + val
+  });
+}
+
+function param_changed(me) {
+  var nid = $(me).parent().parent().parent().parent().parent()[0].id
+    .replace(/[a-zA-Z_]+/, "");
+  var val = $(me)[0].value;
+  var key = $(me)[0].name;
+  var act = "";
+  if (val[0] == "c") { act = "/n_map/"; }
+  else if (val[0] == "a") { act = "/n_mapa/"; }
+  else { act = "/n_set/"; }
+  $.ajax({
+    type: "POST",
+    url: act + nid + "?" + key + "=" + val
+  });
+}
+
+function node_detail(nid) {
+  $.ajax({
+    type: "POST",
+    url: "/nodeDetail/" + nid,
+    success: function(rsp) {
+      $("div.right_main").html(rsp);
+      $(".synth_node").removeClass("active");
+      $("#node_"+nid+"_tree").addClass("active");
+    }
+  });
+}
+
+function focusNodeUp(evt) {
+  current = $(".synth_node.active");
+  if (current.length <= 0) {
+    return;
+  } else {
+    focused = current.prev();
+    if (focused.length <= 0) {
+      focused = current.parent().parent().prev().find(".synth_node").last();
+    }
+    if (focused.length > 0) {
+      node_detail(
+        focused[0].id
+          .replace(/[a-zA-Z_]+/, "")
+          .replace(/[a-zA-Z_]+/, "")
+      );
+    }
+  }
+}
+
+function focusNodeDown(evt) {
+  current = $(".synth_node.active");
+  if (current.length <= 0) {
+    focused = $(".synth_node").first();
+  } else {
+    focused = current.next();
+  }
+  if (focused.length <= 0) {
+    focused = current.parent().parent().next().find(".synth_node").first();
+  }
+  if (focused.length > 0) {
+    node_detail(
+      focused[0].id
+        .replace(/[a-zA-Z_]+/, "")
+        .replace(/[a-zA-Z_]+/, "")
+    );
+  }
+}
+
+function focusFirstInput(evt) {
+  $("input").first().focus();
+}
+
+function init() {
+  $(document).ready(function() {
+    jQuery(document).bind('keypress', 'k', focusNodeUp);
+    jQuery(document).bind('keypress', 'j', focusNodeDown);
+    jQuery(document).bind('keypress', 'i', focusFirstInput);
   });
 }
